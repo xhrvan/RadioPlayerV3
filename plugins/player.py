43ab7d5556dee25651890f03db5530046e5e49ca
SHRVAN <82793048+xhrvan@users.noreply.github.com>
2021-08-23 22:09:02 +0530

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -570,12 +570,12 @@ allcmd = ["play", "current", "playlist", "song", f"song@{USERNAME}", f"play@{USE
 async def not_chat(_, m: Message):
     buttons = [
             [
-                InlineKeyboardButton("CHANNEL", url="https://t.me/AsmSafone"),
-                InlineKeyboardButton("SUPPORT", url="https://t.me/SafoTheBot"),
+                InlineKeyboardButton("CHANNEL", url="https://t.me/Tgxbotz_Update"),
+                InlineKeyboardButton("SUPPORT", url="https://t.me/Tgxbotz_Chat"),
             ],
             [
-                InlineKeyboardButton("🤖 MAKE YOUR OWN BOT 🤖", url="https://heroku.com/deploy?template=https://github.com/AsmSafone/RadioPlayerV3"),
+                InlineKeyboardButton("🤖 MAKE YOUR OWN MUSIC BOT", url="https://github.com/xhrvan/EvilMusic"),
             ]
          ]
-    k=await m.reply_photo(photo="https://telegra.ph/file/4e839766d45935998e9c6.jpg", caption="**Sorry, You Can't Use This Bot In This Group! 🤷‍♂️ But You Can Make Your Own Bot Like This From The [Source Code](https://github.com/AsmSafone/RadioPlayerV3) Below 😉!**", reply_markup=InlineKeyboardMarkup(buttons))
+    k=await m.reply_photo(photo="https://telegra.ph/file/b59e00c649f75fffbcd5d.jpg", caption="**Sorry, You Can't Use This Bot In This Group! 🤷‍♂️ But You Can Make Your Own Bot Like This From The [Source Code](https://github.com/xhrvan/EvilMusic) Below 😉!**", reply_markup=InlineKeyboardMarkup(buttons))
     await mp.delete(m)
